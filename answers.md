f7c35ba58b413a6938fafacd7d969ad1fec0b1ea
sofiadellarosa <142845254+sofiadellarosa@users.noreply.github.com>
2024-04-11 01:21:39 +0000

    Finished

--- answers.md
+++ answers.md
@@ -1,20 +1,24 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Sofia Della Rosa
+**Name:** Jaimie Morris 
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** W(n) = W(n-1) + W(n-2) + O(1)
+  Base n=1
+  W(n) = O(2^n-1) + O(2^n-2) + O(1) = O(2^n)
 
-- **3)**
+- **3)** S(n-1) + 1
+  = O(n)
 
-- **4)**
+- **4)** The values in counts start increasing to the fibonacci numbers but it repeats and rewrites many values that were already calculated before getting the series down correctly.
 
-- **6)**
+- **6)** It will be called n amount of times. Once for each value because instead of calculating the sequence multiple times recursively it will recheck if the value was already calculated. This makes work and span both O(n)
 
-- **8)**
+- **8)** The maximum number of times that $F_i$ will be read for any value $i$ is n-2. The work of fib_bottom_up is O(n) and the span is O(n)
+
--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 def fib_recursive(n, counts):
-    """
+  """
     Return the nth Fibonacci number. 
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
-    """    
-    counts[n] += 1
-    ###TODO
-    pass
-    
+    """
+  counts[n] += 1
+  print(counts)
+  if n == 0:
+    return 0
 
-    
+  elif n == 1:
+    return 1
+
+  return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
+
+
+n = 10
+counts = [0] * (n + 1)
+print(fib_recursive(n, counts))
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+  if n == 0:
+    return 0
+
+  elif n == 1:
+    return 1
+
+  elif n in fibs:
+    return fibs[n]
+
+  else:
+    return fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
 
+  fibs = [0] * (n + 1)
+  fibs[0] = 0
+  fibs[1] = 1
+  for i in range(2, n + 1):
+    fibs[i] = fibs[i - 1] + fibs[i - 2]
 
-
-
-def fib_bottom_up_better(n):
-    ###TODO
-    pass
+  return fibs[n]
